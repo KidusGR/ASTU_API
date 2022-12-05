769f0884efb92241c1569ca1dbc6a5beb23a4f0e
KidusGR <kidmen10@gmail.com>
2022-12-05 19:47:28 +0300

    Update

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ class Stalker:
     def __init__(self):
         self.session = HTMLSession()
         self.headers = payhead.main_header
+        self.baseurl = payhead.baseurl
+        self.graphs = payhead.graphs
 
     def login(self):
         print("--- test ---")
         res = self.session.post(payhead.baseurl, headers=self.headers, json=payhead.login_payload)
+        global resHeaders
         resHeaders = res.headers
-        payhead.yearSemesters.update({
+        print(f"\n{json.loads(res.content)}\n")
+
+    def fetch(self, payload):
+        print("--- Fetching ---")
+        self.payload = payload
+        payload.update({
             'access-token': resHeaders['access-token'],
             'client': resHeaders['client'],
             'uid': resHeaders['uid']
         })
-        print(json.loads(res.content))
-        test = self.session.post(payhead.graphs, headers=self.headers, json=payhead.yearSemesters)
-        print(json.loads(test.content))
+        response = self.session.post(payhead.graphs, headers=self.headers, json=payload)
+        print(f"\n{json.loads(response.content)}\n")
