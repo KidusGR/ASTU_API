ccc52c2a9484f25e25114002c83f26007d765d53
KidusGR <kidmen10@gmail.com>
2022-12-26 01:21:41 +0300

    Update

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
 import main
 
-idnumber = "ugr/23346/13"  # input("Enter Your ID: ")
-password = "Strong098"  # input("Enter Your Password: ")
+idnumber = input("Enter Your ID: ")
+password = input("Enter Your Password: ")
 
 inst1 = main.Stalker()
 inst1.login(idnumber, password)
-
-print("--- fetching ---\n")
-
-# tests
 
 inst1.fetch()
